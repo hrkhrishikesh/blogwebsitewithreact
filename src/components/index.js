b4b225cbb2618a5fe02fc7d6e26d361cd7645ce3
hrkhrkhrk <kumar.hrishikesh27@gmail.com>
2025-03-30 04:10:49 +0530

    mann, did too many things

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
-import Header from "./Header";
+import Header from "../components/Header";
 import Footer from "./Footer";
+import Container from "./Container"
+import Logo from "./Logo";
+import Logoutbtn from "./Logoutbtn"
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    Logoutbtn
 }
